0293881ba0529ec4e588e64b8684abd0dd78f401
leo <leofallensoul@gmail.com>
2019-07-24 23:39:17 +0300

    на пол пути к планшету

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,10 @@
 var headerToggleOpen = document.querySelector(".page-header__toggle");
 var headerToggleClose = document.querySelector(".page-header__toggle-close");
-var mainNavMenu = document.querySelector(".main-nav__list");
+var mainNavMenu = document.querySelector(".main-nav");
 
 headerToggleOpen.addEventListener("click", function(evt) {
     evt.preventDefault();
     headerToggleOpen.classList.toggle("page-header__toggle-close");
-    mainNavMenu.classList.toggle("main-nav__list--none");
+    mainNavMenu.classList.toggle("main-nav--none");
 
 });
