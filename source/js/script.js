aec1edb62c82d9c1d7868714c9cd0b5068b471f2
leo <leofallensoul@gmail.com>
2019-07-24 19:34:59 +0300

    проблема с less

--- source/js/script.js
+++ source/js/script.js
@@ -3,12 +3,8 @@ var headerToggleClose = document.querySelector(".page-header__toggle-close");
 var mainNavMenu = document.querySelector(".main-nav__list");
 
 headerToggleOpen.addEventListener("click", function(evt) {
-  if (headerToggleOpen) {
     evt.preventDefault();
-    headerToggleOpen.classList.add("page-header__toggle-close");
-    mainNavMenu.classList.remove("main-nav__list--none");
-  } else {
-    evt.preventDefault();
-    headerToggleClose.classList.remove("page-header__toggle-close");
-  }
+    headerToggleOpen.classList.toggle("page-header__toggle-close");
+    mainNavMenu.classList.toggle("main-nav__list--none");
+
 });
