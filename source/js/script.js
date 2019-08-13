8a5ddba9e41150cfa88bd60e524cd52bed26476a
leo <leofallensoul@gmail.com>
2019-08-13 16:06:37 +0300

    подпилил реализацию меню без js

--- source/js/script.js
+++ source/js/script.js
@@ -2,15 +2,28 @@ var headerToggleOpen = document.querySelector(".page-header__toggle");
 var headerToggleClose = document.querySelector(".page-header__toggle-close");
 var mainNavMenu = document.querySelector(".main-nav");
 var menuToggle = document.querySelector(".page-header__toggle");
-var viewport = document.documentElement.clientWidth
+var viewport = document.documentElement.clientWidth;
+
 
 if (viewport < 768) {
   mainNavMenu.classList.add("display-none");
   menuToggle.classList.remove("display-none");
-};
+}
+
+window.addEventListener("resize", function() {
+  viewport = document.documentElement.clientWidth;
+
+  if (viewport >= 768) {
+    mainNavMenu.classList.remove("display-none");
+    menuToggle.classList.add("display-none");
+} else {
+  mainNavMenu.classList.add("display-none");
+  menuToggle.classList.remove("display-none");
+}
+});
 
 headerToggleOpen.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    headerToggleOpen.classList.toggle("page-header__toggle-close");
-    mainNavMenu.classList.toggle("display-none");
+  evt.preventDefault();
+  headerToggleOpen.classList.toggle("page-header__toggle-close");
+  mainNavMenu.classList.toggle("display-none");
 });
